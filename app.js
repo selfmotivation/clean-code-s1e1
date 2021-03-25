38e595fbbd387aad53a6e16dcf4457103623c7b0
Selfmotivation <mira55@yandex.ru>
2021-03-26 03:46:50 +0400

    refactor: some more BEM according to rule 2.1 in html-and-css-extended

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.querySelector(".add__input");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.querySelector(".list-incomplete");//ul of #incompleteTasks
 var completedTasksHolder=document.querySelector(".list-completed");//completed-tasks
